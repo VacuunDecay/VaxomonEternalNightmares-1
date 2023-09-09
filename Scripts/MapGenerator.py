e4a1d5ce6a8a8ec0e1b63e7d9e0cc2f3911e6ed9
VacuunDecay <82185824+VacuunDecay@users.noreply.github.com>
2023-09-09 11:13:12 -0300

    TileMap

--- Scripts/MapGenerator.py
+++ Scripts/MapGenerator.py
@@ -1,5 +1,22 @@
-class MapGenerator:
-    @staticmethod
-    def LoadMap():
-        TileX = 64
-        TileY = 64
+from Sprite import Sprite
+
+class MapGenerator():
+    TileSetX = 64
+    TileSetY = 64
+
+    mat = [[0] * 10 for _ in range(10)]
+
+    def __init__(self, scrn):
+        self.mat[2][4] = 1
+        self.scrn = scrn
+        self.img1 = Sprite("Tilesets\\GrassTop.png", self.scrn)
+
+
+    def draw(self):
+        for i in range(0, 10):
+            for j in range(0, 10):
+                if self.mat[i][j] == 1:
+                    self.img1.draw(i*self.TileSetX, j*self.TileSetY)
+                if self.mat[i][j] == 0:
+                    pass
+                    #imagem0 em i*tylesetx j*tylesetY
